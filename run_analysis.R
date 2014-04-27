8bfb315609e6695160caa36ce77ae08cad8e446e
Magi <magizhang@qq.com>
2014-04-27 12:59:48 +0800

    Peer Assessments for coursera

---
+++ run_analysis.R
@@ -0,0 +1,59 @@
+#try guessing working directory for data set
+cat("\nDetermining working directory...")
+if(!file.exists("features.txt")){
+  Dir <- "./UCI HAR Dataset/"
+  while(!file.exists(file.path(Dir, "features.txt"), showWarnings=F)) Dir <- readline(paste("No dataset found in\n",normalizePath(Dir),"\nPlease input path of dataset (folder which containts 'features.txt' etc.):"))
+  setwd(Dir);
+}
+cat("use '",getwd(),"' as working directory.")
+
+#load all data needed to memory
+cat("\nReading dataset...")
+features <- read.table("features.txt", col.names=c("fid", "fname"))
+activities <- read.table("activity_labels.txt", col.names=c("aid", "aname"))
+X_train <- read.table("train/X_train.txt", col.names=features$fname)
+y_train <- read.table("train/y_train.txt", col.names=c("aid"))
+subject_train <- read.table("train/subject_train.txt", col.names=c("subject"))
+X_test <- read.table("test/X_test.txt", col.names=features$fname)
+y_test <- read.table("test/y_test.txt", col.names=c("aid"))
+subject_test <- read.table("test/subject_test.txt", col.names=c("subject"))
+cat("done.")
+
+#1. Merges the training and the test sets to create one data set.
+cat("\nMerging dataset...")
+X<-rbind(X_train,X_test)
+y<-rbind(y_train,y_test)
+subject<-rbind(subject_train,subject_test)
+cat("done.")
+
+#2.Extracts only the measurements on the mean and standard deviation for each measurement
+cat("\nExtracting mean and std...")
+Xsub<-X[,grep("-(mean|std)\\(\\)",features$fname)]
+cat("done.")
+
+#3.Uses descriptive activity names to name the activities in the data set
+cat("\nNaming activities...")
+y[,"aname"]<-activities[y[,"aid"],"aname"]
+cat("done.")
+
+#4.Appropriately labels the data set with descriptive activity names. 
+cat("\nLabeling data set...")
+dataset<-cbind(Xsub,y$aname,subject$subject)
+cat("done.")
+
+#5.Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+cat("\nCreating tidy data set...")
+tidy<-aggregate(Xsub,list(subject=y$aname, activity=subject$subject), mean)
+cat("done.")
+
+#output tidy.txt for upload
+cat("\nWriting tidy data set...")
+write.table(tidy, "tidy.txt", sep="\t")
+cat("write to '",normalizePath("tidy.txt"),"' ok.")
+
+
+cat("\nAll done. Thanks for taking time to check my assessment.")
+
+
+
+
